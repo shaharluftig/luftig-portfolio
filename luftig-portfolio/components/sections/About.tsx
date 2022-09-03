cb312c5b45d07b1a40b64aa649ead4fdb22c042a
Luftig <shaharluftig@gmail.com>
2022-09-03 16:27:09 +0300

    Added about section

--- luftig-portfolio/components/sections/About.tsx
+++ luftig-portfolio/components/sections/About.tsx
@@ -1,18 +1,32 @@
 import Image from "next/image";
-import { useState } from "react";
 import styles from '../../styles/About.module.css';
+
+const technologies = ["Apache Spark", "Python", "Java", "Linux", "RDBMS", "NoSQL"]
+
 export default function About() {
   return (
     <div className={styles.container} id="About">
-
-
       <div className={styles.aboutContainer}>
-        <h2 className={styles.aboutHeader}>About me</h2>
-        <p className={styles.aboutText}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam nulla quia iure quasi. Aut ipsam quam perferendis suscipit tempora cumque, fugiat neque molestias pariatur odit deserunt!
-        </p>
+        <div className={styles.textContainer}>
+          <h2 className={styles.aboutHeader}>About me</h2>
+          <p className={styles.aboutText}>
+            I am a data engineer with experience in designing, building, and maintaining data systems. I have a strong background in computer science and experience with a variety of programming languages and tools. I am a quick learner and have a keen eye for detail. I am passionate about data and love working with it to find insights that can help improve businesses.
+          </p>
+        </div>
+        <Image className={styles.image} src="/images/shahar.jpg" height={250} width={250} />
       </div>
-      <Image className={styles.image} src="/images/shahar.jpg" height={250} width={250} />
+      <div>
+        <h3>Some of the technologies Ive worked with</h3>
+        <ul className={styles.skillsList}>
+          {technologies.map(technology => {
+            return (
+              <li className={styles.skillItem}>
+                {technology}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
 
   );
