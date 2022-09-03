7f5d95496a901a78aa6f4de7e6702754c4340b5f
Luftig <shaharluftig@gmail.com>
2022-09-03 17:01:59 +0300

    Change footer to span

--- luftig-portfolio/components/Footer.tsx
+++ luftig-portfolio/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
 
     return (
         <div className={styles.container}>
-            <p>Created by Shahar Luftig</p>
+            <span>Created by Shahar Luftig</span>
         </div>
     )
 }
