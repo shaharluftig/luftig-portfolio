26071222e05d1e92c8d35c84181408c55f1791fe
Luftig <shaharluftig@gmail.com>
2022-09-03 13:50:39 +0300

    Added image to about section

--- luftig-portfolio/components/sections/About.tsx
+++ luftig-portfolio/components/sections/About.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
+import { useState } from "react";
 import styles from '../../styles/About.module.css';
-
 export default function About() {
   return (
     <div className={styles.container} id="About">
-      <h2 >About me</h2>
+
+
       <div className={styles.aboutContainer}>
+        <h2 className={styles.aboutHeader}>About me</h2>
         <p className={styles.aboutText}>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam nulla quia iure quasi. Aut ipsam quam perferendis suscipit tempora cumque, fugiat neque molestias pariatur odit deserunt!
         </p>
-        <Image className={styles.image} src="/images/shahar.jpg" height={250} width={250} />
       </div>
+      <Image className={styles.image} src="/images/shahar.jpg" height={250} width={250} />
     </div>
 
   );
--- luftig-portfolio/components/sections/Experience.tsx
+++ luftig-portfolio/components/sections/Experience.tsx
@@ -5,17 +5,22 @@ export default function Experience() {
     return (
         <div>
             <div className={styles.container} id="Experience">
-                <h2 >Experience</h2>
-                <p className={styles.about}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam nulla quia iure quasi. 
-                    Aut ipsam quam perferendis suscipit tempora cumque, fugiat neque molestias pariatur odit deserunt!
-                     Quasi voluptatum vero reiciendis earum repellat veritatis consequuntur alias ducimus esse repudianda
-                     e sequi officia temporibus doloremque amet optio nisi, explicabo debitis a quisquam? Porro saepe aperiam
-                      voluptas et dolorum architecto voluptates reprehenderit ut eum nostrum numquam vitae dicta voluptate autem 
-                      tempore, dolores excepturi modi optio eligendi nesciunt! Facere laboriosam ipsam earum blanditiis non, cum 
-                      optio iste quasi explicabo ratione dolores deserunt mollitia impedit expedita alias asperiores sed, perferendis
-                       atque velit ullam, adipisci libero eligendi.
-                </p>
+                <h2 >Where I`ve Worked</h2>
+                <h3>IDF</h3>
+                <ul className={styles.about}>
+                    <li>
+                        Data Enginner
+                    </li>
+                    <li>
+                        Officers Course
+                    </li>
+                    <li>
+                        Data enginner team lead
+                    </li>
+                    <li>
+                        Data devlopers team lead
+                    </li>
+                </ul>
             </div>
 
         </div>
